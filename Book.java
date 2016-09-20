7dfbebcfc633dabec1a9befb3f6fddc4b7eac6aa
nathanpaget <npaget@comcast.net>
2016-09-19 20:23:35 -0400

    Exercise 2.83
    Added getAuthor and getTitle methods

--- Book.java
+++ Book.java
@@ -23,4 +23,12 @@ class Book
     }
 
     // Add the methods here ...
+    public String getAuthor()
+    {
+        return author;
+    }
+    public String getTitle()
+    {
+        return title;
+    }
 }
